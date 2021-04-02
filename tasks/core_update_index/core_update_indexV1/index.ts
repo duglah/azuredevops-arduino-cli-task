ee29ca78bc32a62507cf9a5c25d958b06e71f9b1
Philipp Manstein <philipp.gm@googlemail.com>
2021-04-02 17:06:34 +0200

    Move task in separate version folders
    Support multiple version layout, like described in https://docs.microsoft.com/en-us/azure/devops/extend/develop/integrate-build-task?view=azure-devops

---
+++ tasks/core_update_index/core_update_indexV1/index.ts
@@ -0,0 +1,45 @@
+import path = require('path');
+import task = require('azure-pipelines-task-lib/task');
+import tool = require('azure-pipelines-tool-lib/tool');
+
+const ARDUINOCLI_TOOL_PATH_ENV_VAR: string = "ArdunioCliToolPath";
+const ARDUINOCLI_TOOL_NAME: string = 'arduino-cli';
+
+async function run() {
+    try {
+        let localArduinoCli = task.getVariable(ARDUINOCLI_TOOL_PATH_ENV_VAR) || process.env[ARDUINOCLI_TOOL_PATH_ENV_VAR] || task.getInput(ARDUINOCLI_TOOL_PATH_ENV_VAR, false);
+       
+        if(localArduinoCli === undefined) {
+            task.setResult(task.TaskResult.Failed, `Couldn't find ${ARDUINOCLI_TOOL_PATH_ENV_VAR} variable! Please run arduino-cli install task first.`);
+            return;
+        }
+
+        // Example:
+        // arduino-cli core update-index [flags]
+
+        let fullPath = path.join(localArduinoCli, ARDUINOCLI_TOOL_NAME);
+
+        var args = ["core", "update-index"];
+
+        let additionalUrls = task.getInput("additionalUrls", false);
+        if(additionalUrls != undefined)
+            args.push(`--additional-urls=${additionalUrls}`);
+        
+        let result = await task.exec(fullPath, args);
+        task.debug(`Executed with args ${args}:  '${result}'`);
+        
+        if(result == 0) {
+            task.setResult(task.TaskResult.Succeeded, "Updated core index!");
+            return;
+        }
+        
+        task.setResult(task.TaskResult.Failed, "Error while updating core index!");
+        return;
+    }
+    catch (err) {
+        task.error(err.message);
+        task.setResult(task.TaskResult.Failed, err.message);
+    }
+}
+
+run();
