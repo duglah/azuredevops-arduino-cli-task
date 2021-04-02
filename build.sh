393f070e133058a635422c0640291bb46f6aaaad
Philipp Manstein <philipp.gm@googlemail.com>
2021-04-02 18:04:50 +0200

    Add new paths to build.sh

--- build.sh
+++ build.sh
@@ -2,28 +2,57 @@
 # Build TS
 yarn global add typescript
 
-cd ./tasks/build
+echo "Building Task: build"
+cd ./tasks/build/buildV0
 yarn install
 tsc
-cd ../install
-yarn install
-tsc
-cd ../core_install
-yarn install
-tsc
-cd ../core_update_index
-yarn install
-tsc
-cd ../lib_install
-yarn install
-tsc
-cd ../lib_update_index
+cd ../buildV1
 yarn install
 tsc
 
-cd ../../
+echo "Building Task: install"
+cd ../../install/installV0
+yarn install
+tsc
+cd ../installV1
+yarn install
+tsc
 
-# Create extension
+echo "Building Task: core_install"
+cd ../../core_install/core_installV0
+yarn install
+tsc
+cd ../core_installV1
+yarn install
+tsc
+
+echo "Building Task: core_update_index"
+cd ../../core_update_index/core_update_indexV0
+yarn install
+tsc
+cd ../core_update_indexV1
+yarn install
+tsc
+
+echo "Building Task: lib_install"
+cd ../../lib_install/lib_installV0
+yarn install
+tsc
+cd ../lib_installV1
+yarn install
+tsc
+
+echo "Building Task: lib_update_index"
+cd ../../lib_update_index/lib_update_indexV0
+yarn install
+tsc
+cd ../lib_update_indexV1
+yarn install
+tsc
+
+cd ../../../
+
+echo "Creating extension..."
 yarn global add tfx-cli
 tfx extension create --manifest-globs vss-extension.json
 
