7e7473135a4622125d2371d6a104e72213c2737a
Philipp Manstein <philipp.gm@googlemail.com>
2021-04-03 13:34:38 +0200

    Update compile task

--- tasks/build/buildV1/index.ts
+++ tasks/build/buildV1/index.ts
@@ -24,7 +24,84 @@ async function run() {
         let fqbn = task.getInput("fqbn", true);
         args.push(`--fqbn=${fqbn}`);
 
-        let sketchDir = task.getInput("sketchDir", true);
+        let outputDir = task.getPathInput("outputDir", false);
+        if(outputDir != undefined)
+            args.push(`--output-dir=${outputDir}`);
+
+        // Options
+
+        let buildCachePath = task.getPathInput("buildCachePath", false);
+        if(buildCachePath != undefined)
+            args.push(`--build-cache-path=${buildCachePath}`);
+
+        let buildPath = task.getPathInput("buildPath", false);
+        if(buildPath != undefined)
+            args.push(`--build-path=${buildPath}`);
+
+        let buildProperty = task.getInput("buildProperty", false);
+        if(buildProperty != undefined)
+            args.push(`--build-property=${buildProperty}`);
+
+        let clean = task.getBoolInput("clean", false);
+        if(clean)
+            args.push(`--clean`);
+
+        let exportBinaries = task.getBoolInput("exportBinaries", false);
+        if(exportBinaries)
+            args.push(`--export-binaries`);
+
+        let libraries = task.getInput("libraries", false);
+        if(libraries != undefined)
+            args.push(`--libraries=${libraries}`);
+        
+        let onlyCompilationDatabase = task.getBoolInput("onlyCompilationDatabase", false);
+        if(onlyCompilationDatabase)
+            args.push(`--only-compilation-database`);
+        
+        let optimizeForDebug = task.getBoolInput("optimizeForDebug", false);
+        if(optimizeForDebug)
+            args.push(`--optimize-for-debug`);
+
+        let vidPid = task.getInput("vidPid", false);
+        if(vidPid != undefined)
+            args.push(`--vid-pid=${vidPid}`);
+
+        let warnings = task.getInput("warnings", false);
+        if(warnings != undefined)
+            args.push(`--warnings=${warnings}`);
+        
+        // Options inherited from parent commands
+
+        let additionalUrls = task.getInput("additionalUrls", false);
+        if(additionalUrls != undefined)
+            args.push(`--additional-urls=${additionalUrls}`);
+
+        let configFile = task.getPathInput("configFile", false);
+        if(configFile != undefined)
+            args.push(`--config-file=${configFile}`);
+
+        let format = task.getInput("format", false);
+        if(format != undefined)
+            args.push(`--format=${format}`);
+        
+        let logFile = task.getPathInput("logFile", false);
+        if(logFile != undefined)
+            args.push(`--log-file=${logFile}`);
+
+        let logFormat = task.getInput("logFormat", false);
+        if(logFormat != undefined)
+            args.push(`--log-format=${logFormat}`);
+
+        let logLevel = task.getInput("logLevel", false);
+        if(logLevel != undefined)
+            args.push(`--log-level=${logLevel}`);
+
+        let verbose = task.getBoolInput("verbose", false);
+        if(verbose)
+            args.push(`--verbose`);
+
+        // Sketch to compile
+        let sketchDir = task.getPathInput("sketchDir", true);
         args.push(`${sketchDir}`);
         
         let result = await task.exec(fullPath, args);
